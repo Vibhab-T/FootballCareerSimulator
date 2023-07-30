ecfeb300ac9633ee0a98fa8f7a4af9c09e5b8f42
Vibhab-T <vibhabtimsina86@gmail.com>
2023-07-30 22:48:55 +0545

    added a readme file made a hierarchy of classes which might not be the actual final hierarchy

---
+++ Main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+class Continent{
+    public:
+        string continentName;
+        float permanentContinentCoeff, seasonalContinentCoeff; 
+
+};
+
+class League: public Continent{
+    public:
+        string leagueName;
+        float permanentLeagueCoeff, seasonalLeagueCoeff;
+};
+
+class Club: public League{
+    public:
+        string clubName;
+        float permanentClubCoeff, seasonalClubCoeff;
+};
+
+class Player: public Club{
+    public:
+        string playerName;
+        int playerNumber, position, playerAge, season[20];
+
+        int goals, assists, cleanSheets, apps;
+        float matchRating, averageMatchRating[20], totalAverageMatchRating;
+};
+
+int main(){
+    
+    return 0;
+}
